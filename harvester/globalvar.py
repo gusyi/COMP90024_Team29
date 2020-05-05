a30d3aea7fd12eca0335fd218b15ab2fa1dab3c2
mirrokle <>
2020-05-05 18:31:30 +1000

    Update globalvar.py

--- harvester/globalvar.py
+++ harvester/globalvar.py
@@ -1,33 +1,42 @@
 app_credentials = [
     {
-        'consumer_key': 'zXoReKA7fvCP5CLiC0C2HDI3Y'
-        'consumer_secret': 'dpMPUWNgbc9nwq5ntanSx7DFYDWQo7hH51CHxbauX2iKyczH7r'
-        'access_token': '1252534782577086464-9JfCWpydeEfubm8YTbuW0wOFlAjxQz'
+        'consumer_key': 'zXoReKA7fvCP5CLiC0C2HDI3Y',
+        'consumer_secret': 'dpMPUWNgbc9nwq5ntanSx7DFYDWQo7hH51CHxbauX2iKyczH7r',
+        'access_token': '1252534782577086464-9JfCWpydeEfubm8YTbuW0wOFlAjxQz',
         'access_secret': 'aS7HIw8P5AmTRs9gGyFeXPzQsBTQj6JBQTiw2KFyOzSUF'
     },
     {
-        'consumer_key': 'Kpcsxv6Ej3ZeI7COBomHBrpbk'
-        'consumer_secret': 'zy74z05Gzk9nTBeilWKXbtrm378238aWueRwgFbxrVDigL7UUL'
-        'access_token': '1255838323173543937-FmDeMQosVMVJthvX6xe3vNhFeahd1p'
+        'consumer_key': 'Kpcsxv6Ej3ZeI7COBomHBrpbk',
+        'consumer_secret': 'zy74z05Gzk9nTBeilWKXbtrm378238aWueRwgFbxrVDigL7UUL',
+        'access_token': '1255838323173543937-FmDeMQosVMVJthvX6xe3vNhFeahd1p',
         'access_secret': '83WwwSU6jRk9qirIWE1te8fgnE2oXkjZvhF0hf6k9hubt'
     },
     {
-        'consumer_key': 'XhbWKUWumjzYbTeWI46oCEzTV'
-        'consumer_secret': 'NbFHMGH0hViChxxtFcsZTsORR3UP1s6opsUBFRMWefvjaqugVL'
-        'access_token': '1255745861994176513-5QmcTTibpGINhf4rXfsn0zQlMgD4uV'
+        'consumer_key': 'XhbWKUWumjzYbTeWI46oCEzTV',
+        'consumer_secret': 'NbFHMGH0hViChxxtFcsZTsORR3UP1s6opsUBFRMWefvjaqugVL',
+        'access_token': '1255745861994176513-5QmcTTibpGINhf4rXfsn0zQlMgD4uV',
         'access_secret': 'XQyr4jDjwc2G2VgOZyXQcVLtlYgSiwUdJ3pxF9JLIi2QN'
     },
     {
-        'consumer_key': '0FvE5fDqbh433oDbuPz4iK0R1'
-        'consumer_secret': 'JdB9BmZfBRGGhHLs3kU4HZWQVioonv2hKmBpiOafb8GZp4lLzI'
-        'access_token': '1250637428194283521-Abxm11N9HbFpQgcPXAYPCwJbhCE4vl'
+        'consumer_key': '0FvE5fDqbh433oDbuPz4iK0R1',
+        'consumer_secret': 'JdB9BmZfBRGGhHLs3kU4HZWQVioonv2hKmBpiOafb8GZp4lLzI',
+        'access_token': '1250637428194283521-Abxm11N9HbFpQgcPXAYPCwJbhCE4vl',
         'access_secret': '2OnvW1pNC6LRRLCWVikOD4NIozzsdgfeiGJ5gTy755BWP'
-    }
+    },
+    {
+        'consumer_key': 'hytqImMqUJF9cCFd3YiWQdYJR',
+        'consumer_secret': 'iBw86hwcs4wz02f2GKLKldjhXFwQkI4GcGMElZ3m4Tjze2MMSG',
+        'access_token': '1255838323173543937-J2DYt8Z65qUfU6HjdHHMlkzKlZo2Q1',
+        'access_secret': '7RgC3JU0dQIPdHgDzH8JnNLEtCuI1ajSAnywRRl1qrF9t'
+    },
 ]
 
 app_assignment = {
-    'historical-melbourne': 0
-    'historical-other' : 1
+    'historical-melbourne': 0,
+    'historical-other' : 1,
     'stream': 2
 }
+
+track_words = ['Scott Morrison', 'Morrison', '@ScottMorrisonMP']
+search_terms = '(Scott Morrison) OR Morrison OR @ScottMorrisonMP'
 

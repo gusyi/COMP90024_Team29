c547eb6c3ed2638b3e9a68ae5c6d0db2974ec763
Sunc4127 <42020175+Sunc4127@users.noreply.github.com>
2020-05-05 16:20:34 +1000

    temCommit

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,5 @@
+from leads.models import Lead 
+from rest_frameowrk import viewsets, permissions
+from .serializers import LeadSerializer
+
+# Lead Viewset
---
+++ leadmanager/leads/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    

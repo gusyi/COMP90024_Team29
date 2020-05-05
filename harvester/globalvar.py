a7339de4b7ed6e167ed0c12477c646726f706542
mirrokle <>
2020-05-05 16:09:37 +1000

    added global variable for twitter harvester

---
+++ harvester/globalvar.py
@@ -0,0 +1,27 @@
+app_credentials = [
+    {
+        'consumer_key': 'zXoReKA7fvCP5CLiC0C2HDI3Y'
+        'consumer_secret': 'dpMPUWNgbc9nwq5ntanSx7DFYDWQo7hH51CHxbauX2iKyczH7r'
+        'access_token': '1252534782577086464-9JfCWpydeEfubm8YTbuW0wOFlAjxQz'
+        'access_secret': 'aS7HIw8P5AmTRs9gGyFeXPzQsBTQj6JBQTiw2KFyOzSUF'
+    },
+    {
+        'consumer_key': 'Kpcsxv6Ej3ZeI7COBomHBrpbk'
+        'consumer_secret': 'zy74z05Gzk9nTBeilWKXbtrm378238aWueRwgFbxrVDigL7UUL'
+        'access_token': '1255838323173543937-FmDeMQosVMVJthvX6xe3vNhFeahd1p'
+        'access_secret': '83WwwSU6jRk9qirIWE1te8fgnE2oXkjZvhF0hf6k9hubt'
+    },
+    {
+        'consumer_key': 'XhbWKUWumjzYbTeWI46oCEzTV'
+        'consumer_secret': 'NbFHMGH0hViChxxtFcsZTsORR3UP1s6opsUBFRMWefvjaqugVL'
+        'access_token': '1255745861994176513-5QmcTTibpGINhf4rXfsn0zQlMgD4uV'
+        'access_secret': 'XQyr4jDjwc2G2VgOZyXQcVLtlYgSiwUdJ3pxF9JLIi2QN'
+    }
+]
+
+app_assignment = {
+    'historical-melbourne': 0
+    'historical-other' : 1
+    'stream': 2
+}
+

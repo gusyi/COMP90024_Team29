8fa0653374c14b3043af0961017aa373d063216e
gusyin <30853433+gusyi@users.noreply.github.com>
2020-05-13 21:13:42 +0800

    website file reorganise

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def home(request):
+
+    return render(request, 'frontend/home.html')
+
 def index(request):
 
     return render(request, 'frontend/index.html')

7441bffe2d2eacaf4a51e2c792dac4514276b4ab
gusyin <30853433+gusyi@users.noreply.github.com>
2020-05-20 02:49:36 +0800

    virtual env

---
+++ backend/myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gusyin/Documents/GitHub/COMP90024_Team29/backend/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
